3a5740b1a15bb57f4a05f8fe8915962c684fa769
KewWM <117059354+KewWM@users.noreply.github.com>
2023-01-23 02:12:37 +0800

    Delete Group1_Occ2 directory

--- Group1_Occ2/Application/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module assignment.application {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens assignment.application to javafx.fxml;
-    exports assignment.application;
-}
--- Group1_Occ2/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module assignment.application {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens Javafx to javafx.fxml;
-    exports Javafx;
-}
